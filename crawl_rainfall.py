0d0db54a27e6cb7139e2745fbd9db7b0d2907170
Shane Reustle <me@shanereustle.com>
2017-09-08 13:28:18 -0700

    new data

--- crawl_rainfall.py
+++ crawl_rainfall.py
@@ -1,9 +1,22 @@
-access_token = '02735a29e901b1f3'
-BASE_URL = 'https://api.wunderground.com/api/{access_token}/history_20170905/q/19601.json'
+import requests
+from pprint import pprint
 
+access_token = '02735a29e901b1f3'
+BASE_URL = 'https://api.wunderground.com/api/{access_token}/history_{crawl_date}/q/19601.json'
+
+crawl_date = '20170903'
 url = BASE_URL.format(**{
     'access_token': access_token,
+    'crawl_date': crawl_date
 })
 
-print url
+req = requests.get(url)
+if 'Bad' in req.text:
+    pprint(req.text)
+    raise ValueError
+
+raw_json = req.json()
+
+precip = float(raw_json['history']['dailysummary'][0]['precipm'])
+pprint(precip)
 
