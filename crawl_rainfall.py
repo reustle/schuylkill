682941393ea0dff601e3efb88c385b9be70dbd55
Shane Reustle <me@shanereustle.com>
2017-09-09 11:33:21 -0700

    precip on graph

--- crawl_rainfall.py
+++ crawl_rainfall.py
@@ -1,22 +1,33 @@
 import requests
+import datetime
 from pprint import pprint
 
 access_token = '02735a29e901b1f3'
 BASE_URL = 'https://api.wunderground.com/api/{access_token}/history_{crawl_date}/q/19601.json'
 
-crawl_date = '20170903'
-url = BASE_URL.format(**{
-    'access_token': access_token,
-    'crawl_date': crawl_date
-})
 
-req = requests.get(url)
-if 'Bad' in req.text:
-    pprint(req.text)
-    raise ValueError
+def pull_rainfall(crawl_date):
+    """ Pull rainfall for a date """
+    
+    url = BASE_URL.format(**{
+        'access_token': access_token,
+        'crawl_date': crawl_date
+    })
 
-raw_json = req.json()
+    req = requests.get(url)
+    if 'Bad Request' in req.text:
+        return None
 
-precip = float(raw_json['history']['dailysummary'][0]['precipm'])
-pprint(precip)
+    raw_json = req.json()
+    
+    if not len(raw_json['history']['dailysummary']):
+        return None
+
+    return float(raw_json['history']['dailysummary'][0]['precipm'])
+
+for i in range(1, 32):
+    if i < 10:
+        i = '0' + str(i)
+    
+    print('["2017-09-' + str(i) + 'T00:00:00", ' + str(pull_rainfall('201709' + str(i))) + '],')
 
--- main.js
+++ main.js
@@ -1,38 +1,71 @@
+'use strict';
+
 var ctx = document.getElementById('waterChart').getContext('2d');
 
 var generateChartData = function(limit){
-    var dataset = {
+    var data = {
         labels: [], // FILL
         datasets: [{
-            label: 'Water Level',
+            label: 'gauge',
+            yAxisID: 'gauge',
             borderColor: 'rgb(54, 162, 235)',
             backgroundColor: 'rgb(54, 162, 235)',
             pointRadius: 0,
             fill: false,
-            data: [], // FILL
+            data: [] // FILL
+        },{
+            label: 'precip',
+            yAxisID: 'precip',
+            borderColor: '#e82b53',
+            backgroundColor: '#e82b53',
+            pointRadius: 0,
+            fill: false,
+            data: [] // FILL
         }]
     }
     
-    // Apply limit if necessary
-    var source = waterData;
+    // River Gauge
+    var gaugeSource = waterData;
     if(limit){
-        source = source.slice(limit*-1);
+        gaugeSource = gaugeSource.slice(limit*-1);
     }
     
-    source.forEach(function(reading){
+    gaugeSource.forEach(function(reading){
         var timestamp = moment(reading[0]);
-        dataset.labels.push(timestamp);
-        dataset.datasets[0].data.push(reading[1]);
+        data.labels.push(timestamp);
+        data.datasets[0].data.push(reading[1]);
     })
     
-    return dataset;
+    // Precipitation
+    var precipSource = precipData;
+    if(limit){
+        precipSource = precipSource.slice(limit*-1);
+    }
+    
+    precipSource.forEach(function(reading){
+        var timestamp = moment(reading[0]);
+        
+        if(reading[1] === null){
+            reading[1] = 0;
+        }
+        
+        data.datasets[1].data.push(reading[1]);
+        
+        for(var i = 0 ; i < 23 ; i++){
+            data.datasets[1].data.push(0);
+        }
+    })
+    
+    console.log('chart data:', data);
+    
+    return data;
 }
 
 var timeFormat = 'MM/DD/YYYY HH:mm';
 
 var waterChart = new Chart(ctx, {
     type: 'line',
-    data: generateChartData(31*24),
+    data: generateChartData(31*24 + 192),
     options: {
         responsive: false,
         tooltips: {
@@ -56,12 +89,25 @@ var waterChart = new Chart(ctx, {
                 }
             }, ],
             yAxes: [{
+                id: 'gauge',
+                type: 'linear',
+                position: 'left',
                 ticks: {
                     callback: function(value, index, values) {
                         return value + ' ft';
                     }
                 }
+            },{
+                id: 'precip',
+                type: 'linear',
+                position: 'right',
+                ticks: {
+                    callback: function(value, index, values) {
+                        return value + ' mm';
+                    }
+                }
             }]
+            
         }
     }
 });
