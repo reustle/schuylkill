2bb3431f83b3f9406e7311dec6b8d13d7f83cd8e
Shane Reustle <me@shanereustle.com>
2017-11-11 19:03:50 +0900

    precip crawler update

--- crawl_precip.py
+++ crawl_precip.py
@@ -1,10 +1,14 @@
+#!/usr/bin/env python
+
 import requests
 import datetime
 from pprint import pprint
+from time import sleep
 
 access_token = '02735a29e901b1f3'
 BASE_URL = 'https://api.wunderground.com/api/{access_token}/history_{crawl_date}/q/19601.json'
 
+MONTH = '11'
 
 def pull_rainfall(crawl_date):
     """ Pull rainfall for a date """
@@ -23,11 +27,18 @@ def pull_rainfall(crawl_date):
     if not len(raw_json['history']['dailysummary']):
         return None
 
-    return float(raw_json['history']['dailysummary'][0]['precipm'])
+    precipm = raw_json['history']['dailysummary'][0]['precipm']
+    if precipm == 'T':
+        precipm = 0
+    
+    return float(precipm)
 
 for i in range(1, 32):
     if i < 10:
         i = '0' + str(i)
     
-    print('["2017-09-' + str(i) + 'T00:00:00", ' + str(pull_rainfall('201709' + str(i))) + '],')
+    print('["2017-' + MONTH + '-' + str(i) + 'T00:00:00", ' + str(pull_rainfall('2017'+MONTH + str(i))) + '],')
+    
+    # 10 calls per minute limit
+    sleep(6.5)
 
