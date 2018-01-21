372fc33e0e0984e2de06850d22b5618fa780ace4
Shane Reustle <me@shanereustle.com>
2018-01-21 01:09:31 -0500

    auto commit changes

--- run_all.sh
+++ run_all.sh
@@ -1,3 +1,6 @@
-./crawl_gauge.py ; 
-./crawl_precip.py ; 
-./read_data.py > docs/data.js
+./crawl_gauge.py && 
+./crawl_precip.py &&
+./read_data.py > docs/data.js &&
+git add docs/data.js data.db &&
+git commit -m "Updating data" &&
+git push
